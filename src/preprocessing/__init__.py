c408a979600efe377ff6ce80bfa3059387f86a77
Pomichal <vajk.pomichal@gmail.com>
2020-04-18 14:27:51 +0200

    ADD: export similar books rating

--- src/preprocessing/__init__.py
+++ src/preprocessing/__init__.py
@@ -1,1 +1,1 @@
-from .transform_column import ColumnTransformer, ExportBookShelves, DropColumns, SelectBooksWithNPercentile, ExportAuthorsAverageRating, EncodeCategories, EmptyValuesFilter, ReviewsLanguageFilter, ReviewLengthFilter, TextPreprocessor
+from .transform_column import ColumnTransformer, ExportBookShelves, DropColumns, SelectBooksWithNPercentile, ExportAuthorsAverageRating, EncodeCategories, EmptyValuesFilter, ReviewsLanguageFilter, ReviewLengthFilter, TextPreprocessor, ExportSimilarBooksRating
